65116326cd7ce61ce93ebb4b32a68839511232c5
Alexey Rivkin <arivkin@nvidia.com>
2022-07-07 19:59:23 +0300

    Align Copyright headers with Nvidia standard

--- contrib/buildrpm.sh
+++ contrib/buildrpm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eE
 #
-# Copyright (C) Mellanox Technologies Ltd. 2001-2016.  ALL RIGHTS RESERVED.
+# Copyright (c) 2001-2016, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # See file LICENSE for terms.
 #
 
